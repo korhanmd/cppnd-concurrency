f162aa2480ed8d7e54fedb0cbeccd56e95d897a7
Korhan Mutludoğan <korhanmd@gmail.com>
2022-09-21 10:28:45 +0300

    Fix message

--- randomness_of_events.cpp
+++ randomness_of_events.cpp
@@ -18,7 +18,7 @@ int main() {
     std::cout << "Finished work 1 in main\n";
 
     std::this_thread::sleep_for(std::chrono::milliseconds(50));
-    std::cout << "Finished work 1 in main\n";
+    std::cout << "Finished work 2 in main\n";
 
     // wait for thread to finish
     t.join();
