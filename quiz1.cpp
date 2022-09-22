39faa05071e1442a75d0fc98340af00d4987b144
Korhan Mutludoğan <korhanmd@gmail.com>
2022-09-22 13:47:58 +0300

    Test by commenting out the delay in main

--- quiz1.cpp
+++ quiz1.cpp
@@ -18,7 +18,7 @@ int main() {
     }
 
     // ensure that main does not return before the threads are finished
-    std::this_thread::sleep_for(std::chrono::milliseconds(1)); // simulate work
+    //std::this_thread::sleep_for(std::chrono::milliseconds(1)); // simulate work
 
     std::cout << "End of main is reached" << std::endl;
     return 0;
