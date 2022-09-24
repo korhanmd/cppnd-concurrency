4d62c2b47fd997d44f85dcd5d0650ff793c52726
Korhan Mutludoğan <korhanmd@gmail.com>
2022-09-24 21:04:14 +0300

    Solve the most vexing parse

--- threads_with_function_objects.cpp
+++ threads_with_function_objects.cpp
@@ -10,13 +10,21 @@ public:
 
 int main() {
     // create thread
-    std::thread t(Vehicle()); // C++'s most vexing parse
+    //std::thread t(Vehicle()); // C++'s most vexing parse
+
+    std::thread t1( (Vehicle()) ); // Add an extra pair of paranthesis
+
+    std::thread t2 = std::thread( Vehicle() ); // Use copy initialization
+
+    std::thread t3{ Vehicle() }; // Use uniform initialization with braces
 
     // do something in main()
     std::cout << "Finished work in main \n";
 
     // wait for thread to finish
-    t.join();
+    t1.join();
+    t2.join();
+    t3.join();
 
     return 0;
 }
