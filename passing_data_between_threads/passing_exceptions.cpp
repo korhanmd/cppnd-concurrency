902910c64519cc180714a973b2144bd9b8a646f3
Korhan Mutludoğan <korhanmd@gmail.com>
2022-10-06 16:09:28 +0300

    Use async instead of thread

--- passing_data_between_threads/passing_exceptions.cpp
+++ passing_data_between_threads/passing_exceptions.cpp
@@ -4,27 +4,18 @@
 #include <cmath>
 #include <memory>
 
-void divideByNumber(std::promise<double> &&prms, double num, double denom) {
+double divideByNumber(double num, double denom) {
     std::this_thread::sleep_for(std::chrono::milliseconds(500)); // simulate work
-    try {
-        if (denom == 0)
-            throw std::runtime_error("Exception from thread: Division by zero!");
-        else
-            prms.set_value(num / denom);
-    }
-    catch(...) {
-        prms.set_exception(std::current_exception());
-    }
+    
+    if (denom == 0) throw std::runtime_error("Exception from thread: Division by zero!");
+    
+    return num / denom;
 }
 
 int main() {
-    // create promise and future
-    std::promise<double> prms;
-    std::future<double> ftr = prms.get_future();
-
-    // start thread and pass promise as argument
-    double num = 42.0, denom = 0.0;
-    std::thread t(divideByNumber, std::move(prms), num, denom);
+    // use async to start a task
+    double num = 42.0, denom = 2.0;
+    std::future<double> ftr = std::async(divideByNumber, num, denom);
 
     // retrieve result within try-catch block
     try {
@@ -34,9 +25,6 @@ int main() {
     catch (std::runtime_error e) {
         std::cout << e.what() << std::endl;
     }
-
-    // thread barrier
-    t.join();
 
     return 0;
 }
