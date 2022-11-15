0c0f6a790aad2c7f2d3091e623e30e9fca405848
Korhan Mutludoğan <korhanmd@gmail.com>
2022-11-15 16:17:39 +0300

    Convert vehicles to messages

--- condition_variables_and_message_queues/generic_message_queue.cpp
+++ condition_variables_and_message_queues/generic_message_queue.cpp
@@ -20,16 +20,16 @@ public:
     T popBack() {
         // perform vector modification under the lock
         std::unique_lock<std::mutex> uLock(_mutex);
-        _cond.wait(uLock, [this] { return !_vehicles.empty(); }); // pass unique lock to condition variable
+        _cond.wait(uLock, [this] { return !_messages.empty(); }); // pass unique lock to condition variable
 
         // remove last vector element from queue
-        T v = std::move(_vehicles.back());
-        _vehicles.pop_back();
+        T msg = std::move(_messages.back());
+        _messages.pop_back();
 
-        return v; // will not be copied due to return value optimization (RVO) in C++
+        return msg; // will not be copied due to return value optimization (RVO) in C++
     }
 
-    void pushBack(T &&v) {
+    void pushBack(T &&msg) {
         // simulate some work                                                                                                                                                                                                       
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
@@ -37,15 +37,15 @@ public:
         std::lock_guard<std::mutex> uLock(_mutex);
 
         // add vector to queue
-        std::cout << "   Vehicle #" << v.getID() << " will be added to the queue" << std::endl;
-        _vehicles.push_back(std::move(v));
+        std::cout << "   Message " << msg << " has been sent to the queue" << std::endl;
+        _messages.push_back(std::move(msg));
         _cond.notify_one(); // notify client after pushing new Vehicle into vector
     }
 
 private:
     std::mutex _mutex;
     std::condition_variable _cond;
-    std::deque<T> _vehicles;
+    std::deque<T> _messages;
 };
 
 int main() {
